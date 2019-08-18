c20838cfe7c0155c1673f34a41f74b1d2eb576f4
cnman <marsedely@163.com>
2019-08-19 00:11:04 +0800

    aqs

---
+++ concurrency/src/main/java/com/mooyle/concurrent/example/aqs/SemaphoreExample1.java
@@ -0,0 +1,39 @@
+package com.mooyle.concurrent.example.aqs;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+public class SemaphoreExample1 {
+
+    private final static int threadCount = 200;
+
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        final Semaphore semaphore = new Semaphore(10);
+
+        for (int i = 0; i < threadCount; i++) {
+            final int threadNum = i;
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    test(threadNum);
+                    semaphore.release();
+                } catch (Exception e) {
+                    log.error("error in test method.", e);
+                }
+            });
+        }
+        log.info("finish");
+        executorService.shutdown();
+    }
+
+    private static void test(int threadNum) throws InterruptedException {
+        log.info("{}", threadNum);
+        Thread.sleep(1000);
+    }
+}
